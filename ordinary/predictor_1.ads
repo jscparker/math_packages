2b163d89b0a853ad934bfe38a99aff36775651c3
jscparker <johnscpg@gmail.com>
2019-02-01 19:02:02 +0000

    text edits

--- ordinary/predictor_1.ads
+++ ordinary/predictor_1.ads
@@ -62,7 +62,7 @@
 generic
 
    type Real is digits <>;
-   --  The independent variable.  Its called Time
+   --  The independent variable.  It's called Time
    --  throughout the package as though the differential
    --  equation dY/dt = F (t, Y) were time dependent.  Of cource
    --  it can have any meaning.
